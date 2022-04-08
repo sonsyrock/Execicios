2a1eecb1f2b8df7ea79b09880bc066e86cbe586f
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-08 17:42:15 -0300

    Concluído Variáveis Soma

---
+++ Exercicio1001/Main.java
@@ -0,0 +1,15 @@
+package Exercicio1001;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args){
+
+        Scanner keyboard = new Scanner(System.in);
+        int a = keyboard.nextInt();
+        int b = keyboard.nextInt();
+        int X = a+b;
+       System.out.println("Entre com 2 valores");
+        System.out.println("X = "+X);
+    }
+};
