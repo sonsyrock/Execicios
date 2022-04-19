3a40cdd754633209e79dfb9fca0487f6c9e54e8c
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-18 21:01:08 -0300

    Resolver problema do Double (Big Double)

--- Exercicio1009/Main.java
+++ Exercicio1009/Main.java
@@ -1,11 +1,23 @@
 package Exercicio1009;
 
-import java.io.Exception;
+import java.io.IOException;
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 
 public class Main {
-    Public static void main (String [] args) throws IOException{
-     
-      
+    public static void main (String [] args) throws IOException{
+        Scanner s = new Scanner (System.in);
+        DecimalFormat df = new DecimalFormat(".00");
+
+        //String nome = s.next();
+        double salario = s.nextDouble();
+        double vendas = s.nextDouble();
+
+        double total =  vendas * 15/100;
+
+        System.out.println("TOTAL = " + total);
+
+       
     }
 }
