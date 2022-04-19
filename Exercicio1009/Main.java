42fece0bb66f5ae5336c5ff7e42bfb2c605b04a4
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-19 10:32:12 -0300

    Salário Bonus solução (Concluído) - Locale

--- Exercicio1009/Main.java
+++ Exercicio1009/Main.java
@@ -3,20 +3,25 @@ package Exercicio1009;
 import java.io.IOException;
 import java.util.Scanner;
 import java.text.DecimalFormat;
+import java.util.Locale;
 
 
 public class Main {
     public static void main (String [] args) throws IOException{
+
+        Locale.setDefault(Locale.US); //Foi importado para conseguir efetuar a operação com Porcentagem que não conseguimos com BigDecimal
         Scanner s = new Scanner (System.in);
         DecimalFormat df = new DecimalFormat(".00");
 
-        //String nome = s.next();
+        String nome = s.next();
         double salario = s.nextDouble();
         double vendas = s.nextDouble();
 
-        double total =  vendas * 15/100;
+       
+        double comissao = vendas * 0.15;
+        double total = comissao + salario;
 
-        System.out.println("TOTAL = " + total);
+        System.out.println("TOTAL = " + df.format(total));
 
        
     }
