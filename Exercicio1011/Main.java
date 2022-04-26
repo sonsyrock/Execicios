50db8583587d58df14bbefbdc054686c917563a4
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-26 11:18:11 -0300

    Teste de Commit

---
+++ Exercicio1011/Main.java
@@ -0,0 +1,10 @@
+package Exercicio1011;
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        Scanner s = new Scanner(System.in);
+    }
+}
