6bd65dce63d21f7026ea7b5c24ae7b3fcdeff63f
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-26 11:48:40 -0300

    Esfera - Fix

--- Exercicio1011/Main.java
+++ Exercicio1011/Main.java
@@ -7,18 +7,19 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args)throws IOException {
-        Scanner s = new Scanner(System.in);
-        Locale.setDefault(Locale.US);
-        DecimalFormat df = new DecimalFormat(".000");
+        try (Scanner s = new Scanner(System.in)) {
+            Locale.setDefault(Locale.US);
+            DecimalFormat df = new DecimalFormat(".000");
 
-        
+            
 
-        double R = s.nextDouble();
+            double R = s.nextDouble();
 
-        double pi = 3.14159;
-        
-        double total = (4.0/3) * pi * Math.pow(R,3);
+            double pi = 3.14159;
+            
+            double total = (4.0/3) * pi * Math.pow(R,3);
 
-        System.out.println("VOLUME = " + df.format(total));
+            System.out.println("VOLUME = " + df.format(total));
+        }
     }
 }
