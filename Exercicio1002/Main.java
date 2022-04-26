386342983dbe862ce8c8c0050c9377068861928a
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-26 17:29:36 -0300

    Fix

--- Exercicio1002/Main.java
+++ Exercicio1002/Main.java
@@ -7,13 +7,13 @@ public class Main {
 
     public static final DecimalFormat df = new DecimalFormat("0.0000");
     public static void main (String [] args){
-        Scanner a = new Scanner (System.in);
+        try (Scanner a = new Scanner (System.in)) {
+            double raio = a.nextDouble();
 
-        double raio = a.nextDouble();
+            double area = 3.14159 * raio * raio;
 
-        double area = 3.14159 * raio * raio;
-
-        System.out.println("A=" + df.format(area));
+            System.out.println("A=" + df.format(area));
+        }
     }
 };
 
---
+++ Exercicio1013/Main.java
@@ -0,0 +1,14 @@
+package Exercicio1013;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        try (Scanner s = new Scanner(System.in)) {
+            
+
+
+        }
+    }
+}
