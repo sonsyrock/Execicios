b1903f14073014f98afed0e3944a86f3d3d3e7bf
sonsyrock <gabrisoares1020@hotmail.com>
2022-04-26 11:45:47 -0300

    Esfera

--- Exercicio1011/Main.java
+++ Exercicio1011/Main.java
@@ -2,11 +2,23 @@ package Exercicio1011;
 
 import java.util.Scanner;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args)throws IOException {
         Scanner s = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        DecimalFormat df = new DecimalFormat(".000");
 
-        b
+        
+
+        double R = s.nextDouble();
+
+        double pi = 3.14159;
+        
+        double total = (4.0/3) * pi * Math.pow(R,3);
+
+        System.out.println("VOLUME = " + df.format(total));
     }
 }
